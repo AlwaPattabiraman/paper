b4290d2a062b48191d8b25c1481c7e96ad62e2c1
Alwa P <alwa.pattabiraman@gmail.com>
2024-10-20 18:55:34 +0530

    initital comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BaseChartDirective } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartComponentComponent } from './chart-component/chart-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartComponentComponent
   ],
   imports: [
     BrowserModule,
+    BaseChartDirective,
+    FontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
